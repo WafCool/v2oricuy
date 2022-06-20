13e24551d9c4a5a2f615b869e57a85c68c5b425b
xdast <102565741+xdast@users.noreply.github.com>
2022-06-21 05:34:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ function time() {console.log(chalk.bgBlack(`[ ${moment().format('HH:mm:ss')} ]`)
       await sleep(4000);
       await dupe(auth);
       time();
-      await sleep(8000);
+      await sleep(10001);
       console.log(`Delay Fixed`)
 
     }
