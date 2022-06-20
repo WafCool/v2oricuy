5a72f8ba080db6dc03e485efe607d25cadd67837
xdast <102565741+xdast@users.noreply.github.com>
2022-06-20 21:55:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ function infor() {
 console.log(chalk.bgBlack(`\r[ ${moment().format('HH:mm:ss')} ] ${chalk.white(`User : ${username}`)} | ${chalk.white(`Country : ${country}`)} | ${chalk.blue(`Trophy : ${trophy}`)} | ${chalk.red(`Crown : ${crown}`)}`));
 }
 
-function success() {console.log(chalk.bgBlack(`\r[ ${moment().format('HH:mm:ss')}]),)}
+function success() {console.log(chalk.bgBlack(`[ ${moment().format('HH:mm:ss')} ]`),)}
 
 console.log(`Starting...
 Information about username, country, etc. Will only show up once after 7 crown. `)
