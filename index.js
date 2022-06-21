c34f05b6565b1e249baac5e176381891548edec8
xdast <102565741+xdast@users.noreply.github.com>
2022-06-21 10:14:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -66,17 +66,17 @@ function time() {console.log(chalk.bgBlack(`[ ${moment().format('HH:mm:ss')} ]`)
       infor();
       console.log('');
       await dupe(auth);
-      await sleep(2000);
+      await sleep(3000);
       await dupe(auth);
-      await sleep(2000);
+      await sleep(3000);
       await dupe(auth);
       await sleep(9000);
       await dupe(auth);
-      await sleep(2500);
+      await sleep(3500);
       await dupe(auth);
-      await sleep(2500);
+      await sleep(3500);
       await dupe(auth);
-      await sleep(10000);
+      await sleep(8000);
       console.log('');
 
     }
