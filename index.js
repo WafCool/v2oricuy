6bfd42bc13a89671c0925dd0c5baf1c25d1f365f
xdast <102565741+xdast@users.noreply.github.com>
2022-06-21 19:52:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -32,9 +32,9 @@ By : ${chalk.bold('xDast#8745 | Instagram : @xdast0')} - Credit : @dkmpostor & @
   console.log('');
   console.log(`Free Script`);
   console.log(`Kalo mau recode, silahkan pake sc lama saya, jadilah kreatif!`);
-  console.log(`if you want to recode, use my old script, be creative!`)
+  console.log(`If you want to recode, use my old script, be creative!`)
   console.log('');
-console.log(`Have problem? report to my discord dm [ xDast#8745 ]`);
+console.log(`Have problem? Report to my discord [ xDast#8745 ]`);
 console.log('');
 
   const auth = rs.question('Enter Authentication Code! : ');
@@ -51,32 +51,34 @@ const result = await dupe(auth);
     }
    else if (result.includes('User')) {
 
-function infor() {
 const data = JSON.parse(result);
 const username = data.User.Username;
 const country = data.User.Country;
 const trophy = data.User.SkillRating;
 const crown = data.User.Crowns;
 
-console.log(chalk.bgBlack(`\r[ ${moment().format('HH:mm:ss')} ] ${chalk.white(`User : ${username}`)} | ${chalk.white(`Country : ${country}`)} | ${chalk.blue(`Trophy : ${trophy}`)} | ${chalk.red(`Crown : ${crown}`)}`));
-}
+console.log(`\r[ ${moment().format('HH:mm:ss')} ]`)
+console.log(`\r${chalk.white(`In-Game Name : ${username}`)},`)
+console.log(`\r${chalk.white(`Country : ${country}`)},`)
+console.log(`\r${chalk.blue(`Trophy : ${trophy}`)},`)
+console.log(`\r${chalk.red(`Crown : ${crown}`)},`)
 
 function time() {console.log(chalk.bgBlack(`[ ${moment().format('HH:mm:ss')} ]`),)}
 
       infor();
       console.log('');
       await dupe(auth);
-      console.log(`Success`);
-      await sleep(2500);
+      console.log(`[ Success Duplicating | ${moment().format('HH:mm:ss')} ]`);
+      await sleep(3500);
       await dupe(auth);
-      console.log(`Success`);
-      await sleep(2500);
+      console.log(`[ Success Duplicating | ${moment().format('HH:mm:ss')} ]`);
+      await sleep(3500);
       await dupe(auth);
-      console.log(`Success`);
-      await sleep(2500);
+      console.log(`[ Success Duplicating | ${moment().format('HH:mm:ss')} ]`);
+      await sleep(3500);
       await dupe(auth);
-      console.log(`Success`);
-      console.log(`10 seconds delay starting...`)
+      console.log(`[ Success Duplicating | ${moment().format('HH:mm:ss')} ]`);
+      console.log(`some trophy may be not duped into the account.`);
       await sleep(10000);
       console.log('');
 
